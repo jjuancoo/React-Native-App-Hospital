4c655713f6230dcbf98417417a04620bb467f341
Juanco <juanffo874@gmail.com>
2024-11-02 18:45:56 -0600

    Define las primeras pantallas y las añade al Tab

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { Provider } from 'react-native-paper';
 
 import Splash from './views/layout/Splash';
+import Home from './views/home/Home';
+import Citas from './views/home/Citas';
 
 const Tab = createMaterialBottomTabNavigator()
 
 function App(): React.JSX.Element {
 
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <Provider>
       <NavigationContainer>
-        <Tab.Navigator initialRouteName="Splash">
-          <Tab.Screen name="Splash" component={Splash} />
+        <Tab.Navigator initialRouteName="Home">
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Citas" component={Citas} />
         </Tab.Navigator>
       </NavigationContainer>
     </Provider>
---
+++ views/home/Citas.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-paper'
+
+const Citas = () => {
+  return (
+    <View>
+        <Text>Desde citas</Text>
+    </View>
+  )
+}
+
+export default Citas
