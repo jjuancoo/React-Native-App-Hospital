da26b964405a78cc014d8240f728106af5476e1f
Juanco <juanffo874@gmail.com>
2024-11-02 18:25:38 -0600

    Crea la configuracion base para la navegacion e instala más dependencias

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Provider } from 'react-native-paper';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import Splash from './views/layout/Splash';
+
+const Tab = createMaterialBottomTabNavigator()
 
 function App(): React.JSX.Element {
 
   return (
-    <View>
-      <Text>Hola bienvenido</Text>
-    </View>
+    <Provider>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Splash">
+          <Tab.Screen name="Splash" component={Splash} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
---
+++ views/layout/Splash.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Splash = () => {
+  return (
+    <View>
+    <Text>Splash Screen</Text>
+      {/* Add your loading animation here */}
+    </View>
+  )
+}
+
+export default Splash
