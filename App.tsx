42f415344b11b9f9fe0f3193cf7b5ff06400bb07
Juanco <juanffo874@gmail.com>
2024-11-02 18:54:58 -0600

    Crea la splash y la agrega al inicio

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-native-paper';
 
 import Splash from './views/layout/Splash';
@@ -8,18 +9,30 @@ import Home from './views/home/Home';
 import Citas from './views/home/Citas';
 
 const Tab = createMaterialBottomTabNavigator()
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
 
   const [isLoading, setIsLoading] = useState(true);
 
+  //Desaparece la splash screen
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <Provider>
       <NavigationContainer>
-        <Tab.Navigator initialRouteName="Home">
+        {isLoading ? (
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Splash" component={Splash} />
+          </Stack.Navigator>
+        ) : <Tab.Navigator initialRouteName="Home">
           <Tab.Screen name="Home" component={Home} />
           <Tab.Screen name="Citas" component={Citas} />
-        </Tab.Navigator>
+        </Tab.Navigator>}
       </NavigationContainer>
     </Provider>
   );
