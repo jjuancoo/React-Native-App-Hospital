6e67a09610f35699130db3dc0b89cbe94abf7490
Juanco <juanffo874@gmail.com>
2024-11-06 01:19:04 -0600

    Almacena el correo en el async storage

--- views/auth/SignIn.jsx
+++ views/auth/SignIn.jsx
@@ -3,6 +3,7 @@ import { View, StyleSheet } from 'react-native'
 import { Text, TextInput, Button, Dialog, Portal } from 'react-native-paper'
 import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { isSearchBarAvailableForCurrentPlatform } from 'react-native-screens'
 
 const SignIn = () => {
 
@@ -27,15 +28,20 @@ const SignIn = () => {
       const url = 'https://privilegecare-deploy-gqmt.onrender.com/login/'
       await axios.post(url, user).then((response) => {
         const token = response.data
-        console.log(typeof token);
+        console.log(token);
         //Almacenar los datos en el storage
         AsyncStorage.setItem('token', token).then((response) => {
           console.log('Token almacenado correctamente')
         })
+        AsyncStorage.setItem('correo', Correo_Electronico).then((response) => {console.log('Correo correctamente almacenado')})
       })
       .catch(function (error) {
         console.log('Este es el: ', error);
       });
+
+      // setTimeout(() => {
+      //   AsyncStorage.clear().then(() => {console.log('Se vacion el async')})
+      // }, 5000);
     } catch (error) {
       console.log(error)
     }
