75bba4ffec43994f3103fec1c79bb40a7742f8ce
Juanco <juanffo874@gmail.com>
2024-11-20 20:03:10 -0600

    Primeros estilos a la screen de resultados estudios

--- views/home/ResultadosEstudios.jsx
+++ views/home/ResultadosEstudios.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ScrollView, StyleSheet } from 'react-native'
 import { Text } from 'react-native-paper'
 
 const ResultadosEstudios = () => {
   return (
-    <View>
-        <Text>Desde resultados estudios</Text>
+    <View style={styles.container}>
+        <ScrollView style={styles.scroll}>
+          <Text style={styles.title}>Resultados de estudios</Text>
+        </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,    
+    },
+    scroll: {
+        marginHorizontal: 10
+    },
+    title: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginBottom: 8,
+        padding: 8
+    },
+    titleCard: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 4,
+      padding: 4,
+    },
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      right: 0,
+      bottom: 0,
+    },
+  });
 
 export default ResultadosEstudios
