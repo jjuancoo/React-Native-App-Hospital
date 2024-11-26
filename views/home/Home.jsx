fe6b87e1edd686d182b4cc73aee027c4cc508e8f
Juanco <juanffo874@gmail.com>
2024-11-25 22:52:05 -0600

    Obtiene el nombre del usuario por medio del storage

--- views/home/Home.jsx
+++ views/home/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
 import { View, ScrollView, StyleSheet } from 'react-native'
 import { Text } from 'react-native-paper'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Home = () => {
+
+  const [user, setUser] = useState('')
+
+  //Obtiene el nombre del usuario
+  useEffect(() => {
+    const getNameUser = async () => {
+      try {
+        const result = await AsyncStorage.getItem('nombre')
+        setUser(result)
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    getNameUser()
+  }, [])
+
   return (
     <View>
         <ScrollView>
-          <Text style={styles.title}>Bienvenido a Privilege Care</Text>
+          <View>
+            <Text style={styles.title}>Hola, {user}</Text>
+          </View>
         </ScrollView>
     </View>
   )
@@ -20,8 +39,8 @@ const styles = StyleSheet.create({
       marginHorizontal: 10
   },
   title: {
-      fontSize: 24,
-      fontWeight: 'bold',
+      fontSize: 20,
+      fontWeight: '500',
       marginBottom: 8,
       padding: 10
   },
