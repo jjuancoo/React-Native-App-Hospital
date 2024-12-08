c49ced57cbcd41c0e7b728db70fb0f5373ec9bd5
Juanco <juanffo874@gmail.com>
2024-12-08 16:46:15 -0600

    Instala y renderiza calendarios en la pagina inicial

--- views/home/Home.jsx
+++ views/home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect }from 'react'
 import { View, ScrollView, StyleSheet } from 'react-native'
 import { Text } from 'react-native-paper'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Calendar, LocaleConfig } from 'react-native-calendars'
 
 const Home = () => {
 
@@ -20,11 +21,40 @@ const Home = () => {
     getNameUser()
   }, [])
 
+  LocaleConfig.locales['es'] = {
+    monthNames: [
+      'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 
+      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    ],
+    monthNamesShort: [
+      'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 
+      'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'
+    ],
+    dayNames: [
+      'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'
+    ],
+    dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+    today: "Hoy"
+  }
+  LocaleConfig.defaultLocale = 'es'
+
   return (
     <View>
         <ScrollView>
           <View>
             <Text style={styles.title}>Hola, {user}</Text>
+          </View>
+          <View>
+            <Text>Proximos estudios</Text>
+            <View>
+              <Calendar
+                hideArrows={true}
+                theme={{
+                  backgroundColor: '#ffffff',
+                  textMonthFontWeight: 'bold',
+                }}
+              />
+            </View>
           </View>
         </ScrollView>
     </View>
