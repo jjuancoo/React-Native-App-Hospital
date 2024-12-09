fe794c884ec9b6041205ae12bf4cebb716249018
Juanco <juanffo874@gmail.com>
2024-12-09 00:17:46 -0600

    Mejora esteticamente la vista de configuracion

--- views/home/Configuracion.jsx
+++ views/home/Configuracion.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, ScrollView } from 'react-native'
-import { Text, Button, Dialog, Portal } from 'react-native-paper'
+import { View, StyleSheet, ScrollView, TouchableHighlight, Image} from 'react-native'
+import { Text, Button, Dialog, Portal, Divider } from 'react-native-paper'
 import { useAuth } from '../../context/AuthContext'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useNavigation } from '@react-navigation/native';
 
 const Configuracion = () => {
+
+    const items = [
+      {title: 'Notificaciones', subtitle: 'Todo lo que pasa en radiologia', icon: 'campana'},
+      {title: 'Ayuda', subtitle: 'Respuesta a preguntas más comunes', icon: 'ayuda'},
+      {title: 'Perfil', subtitle: 'Tu correo, nombre, y mucho más', icon: 'usuario'},
+      {title: 'Seguridad', subtitle: 'Cambia tu contraseña y autenticación', icon: 'escudo'},
+    ]
+
+    const icons = {
+      campana: require('../../src/icons/campana.png'),
+      ayuda: require('../../src/icons/ayuda.png'),
+      usuario: require('../../src/icons/usuario.png'),
+      escudo: require('../../src/icons/escudo.png'),
+    };
 
     const navigation = useNavigation()
 
@@ -26,16 +40,32 @@ const Configuracion = () => {
 
         <View style={{marginBottom: 12, padding: 12}}>
           <Text style={styles.subtitle}>Pefil</Text>
-          <Button>
-            Editar Perfil
-          </Button>
-          <Button>
-            Cambiar Contraseña
-          </Button>
-          <Button>
-            Seguridad y Privacidad
-          </Button>
-          
+          {items.map((item, index) => (
+            <>
+              <TouchableHighlight 
+                key={index}
+                underlayColor="transparent"
+                style={{paddingVertical: 14}}
+                onPress={() => console.log('No disponible aún')}
+              >
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Image
+                    source={icons[item.icon]}
+                    style={{
+                      width: 22,
+                      height: 22,
+                      marginRight: 14,
+                    }}
+                  />
+                  <View>
+                    <Text style={styles.itemTitle}>{item.title}</Text>
+                    <Text style={styles.itemSubtitle}>{item.subtitle}</Text>
+                  </View>
+                </View>
+              </TouchableHighlight>
+              <Divider/>
+            </>
+          ))}
         </View>
 
         <View>
@@ -88,9 +118,17 @@ const styles = StyleSheet.create({
         padding: 10
     },
     subtitle: {
-      fontSize: 18,
+      fontSize: 20,
       fontWeight: '800',
       marginBottom: 12
+    },
+    itemTitle: {
+      fontSize: 16,
+      fontWeight: 'bold'
+    },
+    itemSubtitle: {
+      fontSize: 14,
+      color: '#666'
     },
     buttonExit: {
         marginHorizontal: 10
