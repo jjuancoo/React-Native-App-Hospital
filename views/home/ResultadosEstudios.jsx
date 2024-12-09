ede668e82f0975d459eebe70925de07404e75dc5
Juanco <juanffo874@gmail.com>
2024-12-08 22:41:48 -0600

    Crea el boton para refrecar el estado

--- views/home/ResultadosEstudios.jsx
+++ views/home/ResultadosEstudios.jsx
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import { View, ScrollView, StyleSheet, Image } from 'react-native'
-import { Text, Card, FAB } from 'react-native-paper'
+import { Text, Card, FAB, IconButton } from 'react-native-paper'
 import useAxios from '../../api/estudios.api'
 import LoadingStudios from './Screens/LoadingStudios'
 
 const ResultadosEstudios = () => {
 
     const [resultStudies, setResultStudies] = useState([])
+    const [queryAPI, setQueryAPI] = useState(true)
     const axiosInstance = useAxios()
 
     useEffect(() =>{
@@ -14,17 +15,35 @@ const ResultadosEstudios = () => {
             try {
                 const response = await axiosInstance.get('/resultados_estudios')
                 setResultStudies(response.data)
+                setQueryAPI(false)
             } catch (error) {
                 console.error(error)
             }
         }
-        getStudies();
-    }, [])
+        if(queryAPI){
+          getStudies();
+        }
+    }, [queryAPI])
+
+    const refreshStudio = () => {
+      setQueryAPI(true);
+    }
 
   return (
     <View style={styles.container}>
         <ScrollView style={styles.scroll}>
-          <Text style={styles.title}>Resultados de estudios</Text>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 2}}>
+            <Text style={styles.title}>Resultados de estudios</Text>
+            <IconButton
+              icon={({size, color}) => (
+                <Image
+                  source={require('../../src/icons/refrescar.png')}
+                  style={{width: size, height: size, tintColor: color}}
+                />
+              )}
+              onPress={() => refreshStudio()}
+            />
+          </View>
           <View>
             {resultStudies.length == 0 ? (
               <LoadingStudios/>
