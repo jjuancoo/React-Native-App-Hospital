ecff8d86ba2c7ef382237c8489f1763c33538267
Juanco <juanffo874@gmail.com>
2024-12-08 22:46:04 -0600

    Crea las pestañas para editar el resultado y eliminarlo

---
+++ views/home/Screens/DeleteResultado.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-paper'
+
+const DeleteResultado = () => {
+  return (
+    <View>
+        <Text>Desde borrar el resultado</Text>
+    </View>
+  )
+}
+
+export default DeleteResultado
---
+++ views/home/Screens/EditResultado.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-paper'
+
+const EditResultado = () => {
+  return (
+    <View>
+        <Text>Desde editar el resultado</Text>
+    </View>
+  )
+}
+
+export default EditResultado
